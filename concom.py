2b5fe1afb1ad72943e63a4b965e1985531850b40
Sergei Patiakin <sergei.patiakin@gmail.com>
2019-10-08 11:27:22 +0200

    Add usage info

--- concom.py
+++ concom.py
@@ -8,23 +8,31 @@ from conf import get_conf_paths, parse_conf_file, merge_confs
 from output import output, error_output
 from errors import ManagedException
 from records import create_record, read_branch_record
+from usage import usage_string
 
 def main(args):
     conf_paths = get_conf_paths()
     confs = [parse_conf_file(path) for path in conf_paths]
     conf = merge_confs(confs)
-    if args[0] == 'conf':
+    if not args:
+        output(usage_string)
+    elif args[0] in ('conf', 'config'):
+        if len(args) > 1:
+            raise ManagedException('Unexpected additional arguments: {0}'.format(args[1:]))
         output(json.dumps(conf, indent=4))
-    elif args[0] == 'blank':
-        record_id, record_path = create_record(conf, None)
-        output(record_id)
     elif args[0] in ('feat', 'fix', 'manual'):
         record_fields = {"type": args[0]}
-        if len(args) >= 3 and args[1] == '-m':
+        if len(args) >= 2:
+            if args[1] != '-m':
+                raise ManagedException('Unexpected argument: {0}'.format(args[1]))
+            if len(args) > 3:
+                raise ManagedException('Unexpected additional arguments: {0}'.format(args[3:]))
             record_fields["description"] = args[2]
         record_id, record_path = create_record(conf, record_fields)
         if conf['editor'] is not None:
             call_editor(conf['editor'], record_path)
+        else:
+            output('No editor specified! Record at {0}'.format(record_path))
         output(record_id)
         if conf['auto_add']:
             add_file(record_path)
@@ -35,9 +43,11 @@ def main(args):
         elif len(args) == 2:
             base_treeish = args[1]
             comparison_treeish = 'HEAD'
-        elif len(args) >= 3:
+        elif len(args) == 3:
             base_treeish = args[2]
             comparison_treeish = args[1]
+        elif len(args) > 3:
+            raise ManagedException('Unexpected additional arguments: {0}'.format(args[3:]))
         added_record_ids, removed_record_ids = get_diff(conf, comparison_treeish, base_treeish)
         output('Changes in {0} compared to {1}:'.format(comparison_treeish, base_treeish))
         added_records = [
@@ -60,7 +70,7 @@ def main(args):
         removed_records.sort(key=lambda r: r['content'])
         for record in removed_records:
             lines = record['content'].splitlines()
-            if lines > 0:
+            if lines:
                 output('- ' + lines[0])
             for line in lines[1:]:
                 output('-     ' + line)
@@ -72,3 +82,4 @@ if __name__ == "__main__":
         main(sys.argv[1:])
     except ManagedException as e:
         error_output(str(e))
+        sys.exit(1)
